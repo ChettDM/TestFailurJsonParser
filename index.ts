77cafb2d056865457998432fd65ca7404460e2ea
Chett Michael <Chett.Michael@gmail.com>
2023-10-02 22:18:47 -0500

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+import * as fs from 'fs';
+
+const dataset = {};
+
+// Prepare data for CSV
+let data = '';
+dataset.results.forEach((item, index) => {
+    data += `"${item.automatedTestStorage.replace('"', '""')}","${item.testCaseTitle.replace('"', '""')}""\n`;
+});
+
+// Write data to CSV file
+fs.writeFile('failing-test-cases.csv', data, (err) => {
+    if (err) {
+        console.log('Error writing file:', err);
+    } else {
+        console.log('File written successfully');
+    }
+});
